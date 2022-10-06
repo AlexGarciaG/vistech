cc24ac54f342c65d65a60d2850370cd74fe3a37b
D3ceiver <rodgutzal@gmail.com>
2022-10-06 13:01:24 -0500

    Update diff_drive_node.ino

--- arduino/Diff_vel_node/diff_drive_node/diff_drive_node.ino
+++ arduino/Diff_vel_node/diff_drive_node/diff_drive_node.ino
@@ -6,7 +6,8 @@
 
 #include <ros.h>
 #include <geometry_msgs/Twist.h> // Type of message for the cmd_vel
-
+#include <std_msgs/Float32.h> //Type of messages for /Wl and /Wr topics
+ 
 ros::NodeHandle  nh; //This structure is necessary to work with ROS (init the node, create publishers, subscribers, etc)
 
 void cmd_vel_cb( const geometry_msgs::Twist& vel_msg){
@@ -14,16 +15,25 @@ void cmd_vel_cb( const geometry_msgs::Twist& vel_msg){
 }
 
 ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", &cmd_vel_cb );
+std_msgs::Float32 wl_msg, wr_msg;
+ros::Publisher wl_pub("wl", &wl_msg);
+ros::Publisher wr_pub("wr", &wr_msg);
 
 void setup()
 { 
   pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(cmd_vel_sub);
+  nh.advertise(wl_pub); //Init the publisher to /wl topic
+  nh.advertise(wr_pub); //Init the publisher to /wr topic
 }
 
 void loop()
 {  
+  wl_msg.data = 2.0; //Add some value to the /wl message
+  wr_msg.data = 3.1; //Add some value to the /wr message
+  wl_pub.publish( &wl_msg ); //Publish the message to the /wl topic
+  wr_pub.publish( &wr_msg ); //Publish the message to the /wr topic
   nh.spinOnce(); //Very, very, very improtant to execute at least once ever cylce.
-  delay(1);
+  delayMicroseconds(250);
 }
